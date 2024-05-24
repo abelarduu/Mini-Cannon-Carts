46b8c0c06952d5bd78ffe136c7380e5b4184db9f
Abel Lucas <abelucas009@gmail.com>
2024-05-23 22:04:13 -0300

    1º tentativa de um "Game Over"

--- scripts/Main_scene.gd
+++ scripts/Main_scene.gd
@@ -7,6 +7,9 @@ var player2_position: Vector2
 func _physics_process(_delta):
 	if check_player(player1): player1_position= player1.position
 	if check_player(player2): player2_position= player2.position
+	
+	if not check_player(player1) or not check_player(player2):
+		$game_over_sprite.visible= true
 
 func check_player(player) -> bool:
 	if is_instance_valid(player):
