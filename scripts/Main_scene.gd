81b68a3baceb7890162a0907b03b13c3e144cf02
Abel Lucas <abelucas009@gmail.com>
2024-05-26 21:42:46 -0300

    animação da placa caindo

--- scripts/Main_scene.gd
+++ scripts/Main_scene.gd
@@ -9,13 +9,16 @@ func _physics_process(_delta):
 	if check_player(player1): player1_position= player1.position
 	if check_player(player2): player2_position= player2.position
 	
-	#HUD	
+	#HUD
 	if play: $HUD/game_icon.visible= false
+	if $HUD/game_icon.global_position <=  Vector2(491.020996, 295):
+		$HUD/game_icon.position+= Vector2(0, 5)
+			
 	$HUD/Player1_Scores.text= str(player1.scores)
 	$HUD/Player2_Scores.text= str(player2.scores)
 	if not check_player(player1) or not check_player(player2):
 		$HUD/game_over_sprite.visible= true
-			
+
 func _input(event):
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("ui_select"):
